95dc5b51a901d862a3b1f2c2ccd520f977f87226
Raiymbek <76900558+Raimbek-pro@users.noreply.github.com>
2025-06-20 18:26:46 +0500

    notes details added

---
+++ Notebook LM/Modules/Notes/NotesDetail/NotesDetailBuilder.swift
@@ -0,0 +1,14 @@
+//
+//  NotesDetail.swift
+//  Notebook LM
+//
+//  Created by Райымбек Омаров on 20.06.2025.
+//
+
+//import SwiftUI
+//
+//enum builder {
+//    static func build() -> some View {
+//        return view
+//    }
+//}
